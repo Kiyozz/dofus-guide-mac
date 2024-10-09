e46c35184ce7c0c4197742fac2b704edf641fc92
Paul Tedesco <paul.tedesco@live.fr>
2024-10-10 00:25:30 +0200

    ♻️ refactor(Container.vue): update defineProps to include noBg prop and remove unnecessary bg-blur class from NuxtScrollbar component

--- components/Container.vue
+++ components/Container.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-defineProps({
-  withHeader: Boolean
-})
+defineProps<{
+  noBg?: boolean
+  withHeader?: boolean
+}>()
 </script>
 
 <template>
-  <NuxtScrollbar tag="div" class="px-3 py-4 mt-6 overflow-x-hidden bg-blur">
+  <NuxtScrollbar tag="div" class="px-3 py-4 mt-6 overflow-x-hidden" :class="!noBg ? 'bg-blur' : ''">
     <div v-if="withHeader" class="bg-gray-900 fixed top-0 h-20 w-full -ml-2.5"></div>
     <div class="ml-auto z-10">
       <slot></slot>
